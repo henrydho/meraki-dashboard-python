3b70e552aadef8ac2b99a746bbbe1d0602a7a016
Henry Ho <henry.dh.ho@gmail.com>
2020-02-25 16:14:48 -0800

    Add and enhance functions of utilities/utils.py.

--- src/meraki_dashboard_python/utilities/utils.py
+++ src/meraki_dashboard_python/utilities/utils.py
@@ -1,22 +1,22 @@
-#!/u:sr/bin/env python
+#!/usr/bin/env python
 """Import required modules."""
 import os
 import re
-from typing import TypedDict
+from typing import TypedDict, Tuple
 import meraki
 
 # Constant variables declaration
 API_KEY = os.environ['MERAKI_API_KEY_HH']
 # API_KEY = os.environ['MERAKI_API_KEY_SYD_TRAINING']
 BASE_URL = "https://api.meraki.com/api/v0"
-DEVICE_CODES = tuple(('MX', 'MS', 'MR', 'MV', 'MG', 'SM'))
-NET_TYPES = tuple((
-    {'mx': 'appliance'},
-    {'ms': 'switch'},
-    {'mr': 'wireless'},
-    {'mv': 'camera'},
-    {'sm': 'systemsManager'},
-    {'mg': 'cellularGateway'}))
+PRODUCT_TYPES = {
+    'mx': 'appliance',
+    'ms': 'switch',
+    'mr': 'wireless',
+    'mv': 'camera',
+    'sm': 'systemsManager',
+    'mg': 'cellularGateway'
+    }
 """
 timeZone (string): The timezone of the network.
 Refer to 'TZ' column in the table in en.wikipedia.org
@@ -26,7 +26,6 @@ DEFAULT_TIME_ZONE = 'Australia/NSW'
 
 def validate_net_name(net_name: str) -> str:
     """Validate a network name.
-
     ** Network name can only contain letters, numbers, spaces, and
     these characters: [.@#_-]
 
@@ -46,12 +45,11 @@ def validate_net_name(net_name: str) -> str:
 
 def validate_tags(tags: str) -> str:
     """Validate tags.
-
     ** Tags can contain only letters, numbers, dashes, underscores,
     and periods.
 
-    - tags (string): a list of tags separated by space
-    -> Return network tags: str if tags are valid. Otherwise, raise ValueError.
+    - tags (string): a list of tags separated by space.
+    -> Return the network tags if tags are valid. Otherwise, raise ValueError.
     """
     invalid_chars_regex = re.compile(r'[~`!@#$%^&*()+={}\[\]|\\/:"\',<>?]')
     if invalid_chars_regex.search(tags) is None:
@@ -62,113 +60,78 @@ def validate_tags(tags: str) -> str:
 
 
 def validate_device_code(device_code: str) -> str:
-    """Check valid device code.
-
+    """Validate valid device code.
     ** Valid device codes: [MX, MS, MR, MV, MG, SM]
-    - device_code: string
-    -> Return device code in lower case if valid. Otherwise, raise ValueError
+
+    - device_code (string): device code
+    -> Return device code in lower case if valid. Otherwise, raise ValueError.
     """
-    if (device_code.strip()).upper() in DEVICE_CODES:
+    if not device_code.strip():
+        raise ValueError("Data Error: Hardware type can't be a blank value!")
+    if (device_code.strip()).lower() in PRODUCT_TYPES.keys():
         return (device_code.strip()).lower()
     raise ValueError(
-        f"Data Error: Invalid device code '{device_code.upper()}'! "
-        f"Valid device codes are {DEVICE_CODES}.")
+        f"Data Error: Invalid device code '{device_code}'! "
+        'Valid device codes are '
+        f"{[k.upper() for k, v in PRODUCT_TYPES.items()]}.")
 
 
-def validate_net_type(device_code: str) -> str:
-    """Get network type based on a device code.
+def validate_net_type(net_type: str) -> str:
+    """Validate network types.
+    ** Valid network types are: [
+        'appliance',
+        'switch',
+        'wireless',
+        'camera',
+        'systemsManager',
+        'cellularGateway'
+        ]
 
-    ** Valid network types:
-    ['appliance','switch','wireless','camera', 'system manager',
-    'meraki gateway']
-
-    - device_code (string): [MX, MS, MR, MV, MG, SM]
-    -> Return the name of network type if device_code matched.
-    -> Raise ValueError if there is no nework type matched with device_code.
+    - net_type (string):
+        + a specific nework type, or
+        + a combined network with the network types separated by space.
+    -> Return a string value of netowrk types.
+    -> Raise ValueError if network type is valid.
     """
-    try:
-        device_code = validate_device_code(device_code)
-    except ValueError as err:
-        print(f'-> {err}')
-    else:
-        # Iterate throught NET_TYPES tubble
-        # and get the value of network type.
-        for a_dict in NET_TYPES:
-            for key, value in a_dict.items():
-                if key == device_code:
-                    return value
+    invalid_chars_regex = re.compile(r'[~`!@#$%^&*()-_+={}\[\]|\\/:"\',<>?.]')
+    if invalid_chars_regex.search(net_type) is None:
+        net_types = net_type.split()
+        for _type in net_types:
+            if _type not in PRODUCT_TYPES.values():
+                raise ValueError(
+                    'Data Error: Network types contain invalid value. '
+                    'Valid network types are '
+                    f"{[v for k, v in PRODUCT_TYPES.values()]}")
+        return ' '.join(net_types)
     raise ValueError(
-        f"Data Error: There is no network type defined by the device "
-        f"code '{device_code.upper()}'.")
+        "Data Error: Network types contain only alphbetical characters.")
 
 
-def get_filtered_orgs(orgs: list, org_name: str = None) -> list:
-    """Get a list of organizations filtered by organization name.
+def get_dict_values(dict_keys: list, a_dict: dict) -> list:
+    """Get the dictionary's values from a provided list of dictionary's keys.
 
-    - orgs: organization list object
-    - org_name: string
-    -> Return a specific organization or a list of all organizations
-        if organzation name is an empty string.
-    -> Raised ValueError if organization name does not exist.
+    - dict_keys (list): a list of dictionary's keys to be filtered out from a
+      dictionary _dict.
+    _ a_dict (dict): a dictionary to be filtered with a dict_list.
+    -> Return a list of values from a_dict filltered by dict_keys.
     """
-    # Get a complete organizations list() without filtering by org_name.
-    if not org_name:  # org_name is an empty string
-        return orgs  # Original organizations list
-
-    # Get a list of organizations filtered by organization name.
-    # Note: Orginazation name is not unique and not case sensitive.
-    filtered_orgs = list()
-    for org in orgs:
-        if org_name == org['name']:
-            org_dict = dict(id=org['id'], name=org['name'], url=org['url'])
-            filtered_orgs.append(org_dict)
-
-    if filtered_orgs:
-        return filtered_orgs
-    raise ValueError(
-        f"Data Error: The organization '{org_name}' does not exist!")
-
-
-def get_org_networks(dashboard: meraki.DashboardAPI, org_name: str) -> list:
-    """Get organization networks filtered by organization name.
-
-    - dashboard (meraki.DashboardAPI object): authenticated DashboardAPI
-      session.
-    - org_name (string): organization name
-    -> Return a list of networks of the org_name.
-    """
-    try:
-        orgs = dashboard.organizations.getOrganizations()
-    except UnboundLocalError as err:
-        print(
-            'Meraki API key error: '
-            f'API key contains whitespace characters - {err}')
-    except meraki.APIKeyError as err:
-        print(f'-> {err}')
-    except meraki.APIError as err:
-        print(f'-> {err}')
-    else:
-        try:
-            org = get_filtered_orgs(orgs, org_name)
-        except ValueError as err:
-            print(f'-> {err}')
-        else:
-            org_id = org['id']
-            org_networks = dashboard.networks.getOrganizationNetworks(org_id)
-            return org_networks
+    a_list = list()
+    for key in dict_keys:
+        a_list.append(next((v for k, v in a_dict.items() if k == key), None))
+    return a_list
 
 
 def init_dashboard_session(authentication) -> TypedDict(
-        'DashSession', {'dashboardAPI': meraki.DashboardAPI,
-                        'organizations': list}):
+        'DashboardSession', {'dashboardAPI': meraki.DashboardAPI,
+                             'organizations': list}):
     """Get authenticated DashboardAPI session.
+    ** Note: meraki.APIKeyError only validates blank API key, but does not
+      Validate whitepsace characters and throwing Exception.
 
     - authentication: authentication value
     -> Return a dict() including an authenticated meraki.DashboardAPI object,
        and authorized organizations list if authenticated.
     -> Raise ValueError if API key is not authorised.
-    * Note: meraki.APIKeyError only validates blank API key, but does not
-      Validate whitepsace characters and throwing Exception.
     """
     try:
         dashboard = meraki.DashboardAPI(
@@ -181,4 +144,94 @@ def init_dashboard_session(authentication) -> TypedDict(
     else:
         return {'dashboardAPI': dashboard, 'organizations': orgs}
     raise ValueError(
-        'Authentication error: API key is not authorized!')
+        'Authentication Error: API key is not authorized!')
+
+
+def filter_orgs(orgs: list, org_name: str,
+                unique_org: bool = False) -> Tuple[list, dict]:
+    """Get a list of organizations filtered by organization name.
+    ** Note: Orginazation name is not unique and not case sensitive.
+
+    - orgs (list): organization list object
+    - org_name (string): organization name
+    - unique_org (bool): specify if org_name to be uniquely filtered.
+    -> Return a unique organization or a list of all organizations having the
+       same organization name.
+    -> Raised UserWarning if org_name is not unique and unique_org set to True.
+    -> Raised ValueError if a provided organization name  does not exist.
+    """
+    # Get a list of organizations filtered by organization name.
+    filtered_orgs = [
+        dict(id=org['id'], name=org['name'], url=org['url'])
+        for org in orgs if org_name == org['name']
+        ]
+    if len(filtered_orgs) == 1:
+        return filtered_orgs[0]
+    if len(filtered_orgs) > 1:
+        if unique_org:
+            raise UserWarning(
+                'Ambiguous Value: There are more than one organization '
+                f"named as '{org_name}'!")
+        return filtered_orgs
+    raise ValueError(
+        f"Data Error: The organization named '{org_name}' does not exist!")
+
+
+def get_org_networks(dashboard: meraki.DashboardAPI, org_name: str) -> list:
+    """Get organization's networks filtered by an organization name.
+    ** Note: organization name needs to be unique.
+
+    - dashboard (meraki.DashboardAPI object): authenticated DashboardAPI
+      session.
+    - org_name (string): organization name
+    -> Return a list of networks belonging to a provided unique org_name.
+    """
+    try:
+        orgs = dashboard.organizations.getOrganizations()
+    except UnboundLocalError as err:
+        print(
+            '-> Meraki API key error: '
+            f'API key contains whitespace characters - {err}')
+    except meraki.APIKeyError as err:
+        print(f'-> {err}')
+    except meraki.APIError as err:
+        print(f'-> {err}')
+    else:
+        try:
+            # Get a unique organiation
+            org = filter_orgs(orgs=orgs, org_name=org_name, unique_org=True)
+        except UserWarning as warn:
+            print(f'-> {warn}')
+        except ValueError as err:
+            print(f'-> {err}')
+        else:
+            return dashboard.networks.getOrganizationNetworks(org['id'])
+
+
+def get_networks(org_networks: list, net_name: str,
+                 net_type: int = 0) -> Tuple[dict, list, None]:
+    """Get the networks of a specific organization by network name.
+    ** Note: a combined and standalone network type can have the same network
+    name. In other words, the network name is only unique within a combined or
+    standalone network type context.
+
+    - org_networks (list): an organization's networks list
+    - net_name (string): network name to be filtered
+    - net_type (integer): network type to be filtered
+        - Combined network type: 2
+        - Standalone network type: 1
+        - Default network type: 0 (both network types)
+    -> Return a specific network as a dictionary value or a list of networks
+       if a provided network name (net_name) exist. Otherwise, return None.
+    """
+    if net_type == 2:  # Filter only combined network type
+        return next((
+            net for net in org_networks
+            if net['name'] == net_name and len(net['productTypes']) > 1), None)
+    if net_type == 1:  # Filter only standalone network type
+        return next((
+            net for net in org_networks if net['name'] == net_name and
+            len(net['productTypes']) == 1), None)
+    if net_type == 0:  # Filter both combined and standalone network type
+        return list(net for net in org_networks if net['name'] == net_name)
+    return None
